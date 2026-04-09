93eb1a503c61aead8200e32e81b7b2300e17d713
kbgBG <105027220+kbgBG@users.noreply.github.com>
2026-04-09 20:00:42 +0900

    Update DormBeApplication.java

--- src/main/java/com/dorm/be/DormBeApplication.java
+++ src/main/java/com/dorm/be/DormBeApplication.java
@@ -1,13 +1,13 @@
 package com.dorm.be;
 
+import com.dorm.fw.DormUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DormBeApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DormBeApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        System.out.println(DormUtil.hello());
+        SpringApplication.run(DormBeApplication.class, args);
+    }
 }
